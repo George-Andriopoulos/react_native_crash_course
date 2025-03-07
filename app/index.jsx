8eb1fcc0c1c6fabee0c623a59728a67bf7e486f8
FixCrap <george_andriopoulos@hotmail.gr>
2025-03-08 00:13:05 +0200

    Installed NativeWinds

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+	return (
+		<View className=''>
+			<Text>index</Text>
+		</View>
+	);
+};
+
+export default index;
